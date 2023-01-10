3842a112333e925bbfd8af703d14c90fae53ec54
Gordon D <gordon.draine97@yahoo.com>
2023-01-09 21:16:44 -0500

    adding new link to render on the post card

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -17,6 +17,29 @@ const PostCard = ({ post }) => {
 
         {/* re add the syntax src={post.featuredImage.url} when render issue is resolved. */}
       </div>
+      <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
+        <Link href={`/post/${post.slug}`}>
+          {post.title}
+
+        </Link>
+      </h1>
+      <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
+        <div className='flex items-center justify-center mb-4 lg:mb-0 q-full lg:w-auto mr-8'>
+          <img 
+          alt={post.author.name}
+          height="30px"
+          width="30px"
+          classname="align-middle rounded-full"
+          src={post.author.photo.url}
+          />
+          <p className='inline align-middle text-gray-700 ml-2 text-lg'>
+            {post.author.name}
+          </p>
+        </div> 
+<div className='font-medium text-gray-700'>
+{/* date svg goes in here  */}
+</div>
+      </div>
     </div>
   );
 }
