c61cf7bc0ecaca215c662b4c83bef1bfc69fd091
Gordon D <gordon.draine97@yahoo.com>
2023-01-14 21:11:50 -0500

    finishing the postwidget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -20,8 +20,24 @@ const PostWidget = ({ categories, slug }) => {
   console.log(relatedPosts);
 
   return (
-    <div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+          {slug ? 'Related Posts' : 'Recent Posts'}
+        </h3>
+        {relatedPosts.map((post) => (
+          <div key={post.title} className='flex items-center w-full mb-4'>
+            <div className='w-16 flex-none'>
+                <img alt={post.title}
+                height="60px"
+                width="60px"
+                className='align-middle rounded-full'
+                src={post.featuredImage.url}>
+                </img>
+                {/* probaBly the same problem with inlcunding the .url notation on the end of this line on 34 */}
 
+            </div>
+          </div>
+        ))}
     </div>
   )
 }
