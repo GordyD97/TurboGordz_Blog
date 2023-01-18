84e25d61923a72ce73814e096e46fd1bdfa3d294
Gordon D <gordon.draine97@yahoo.com>
2023-01-18 00:15:54 -0500

    adding momentjs to post widget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,45 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import moment from 'moment';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
 
-import { getRecentPosts, getSimlarPosts } from '../services';
+import { getRecentPosts, getSimlarPosts } from "../services";
 
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
 
   useEffect(() => {
-    if(slug) {
-      getSimilarPosts(categories, slug)
-        .then((result) => setRelatedPosts(result))
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
     } else {
       getRecentPosts(categories, slug).then((result) =>
         setRelatedPosts(result)
-      )
+      );
     }
-  }, [slug])
+  }, [slug]);
   console.log(relatedPosts);
 
   return (
-    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
-        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-          {slug ? 'Related Posts' : 'Recent Posts'}
-        </h3>
-        {relatedPosts.map((post) => (
-          <div key={post.title} className='flex items-center w-full mb-4'>
-            <div className='w-16 flex-none'>
-                <img alt={post.title}
-                height="60px"
-                width="60px"
-                className='align-middle rounded-full'
-                src={post.featuredImage.url}>
-                </img>
-                {/* probaBly the same problem with inlcunding the .url notation on the end of this line on 34 */}
-
-            </div>
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            ></img>
+            {/* probaBly the same problem with inlcunding the .url notation on the end of this line on 34 */}
           </div>
-        ))}
-    </div>
-  )
-}
+          <div className="flex-groq ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format('MMM DD YYYY')}
+            </p>
+            <Link href={`/post/${post.slug}`} key={post.title} className="text-md">
+              {post={title}}
+            </Link>
 
-export default PostWidget
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostWidget;
